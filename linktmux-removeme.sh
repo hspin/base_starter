da6658887e4eb4a69e058cdd530298d487428941
GS XPS <me.gsbit@hsx5.com>
2019-10-08 23:06:12 -0400

    initial commit

---
+++ linktmux-removeme.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR=$(pwd)
+HNAME=${PWD##*/}
+
+sed -i "s/uno11/${HNAME}/" .general-tmuxinator.yml
+
+sed -i "s|dos22|${DIR}/|" .general-tmuxinator.yml
+
+ln -s $DIR/.general-tmuxinator.yml $HOME/.tmuxinator/$HNAME.yml
+
+echo "tmuxinator is ready"
+echo "*******************************"
