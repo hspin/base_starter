465ebcfeb4699c01f5a9b04dd7de5d9e195b6bf8
GSalazar <gs@hspindesign.com>
2022-08-15 10:37:34 -0400

    update eslint

--- setup-prettier.sh
+++ setup-prettier.sh
@@ -3,3 +3,15 @@
 DIR=$(pwd)
 
 npm install --save-dev prettier eslint eslint-config-prettier eslint-plugin-prettier
+
+npm install --save-dev eslint-plugin-tailwindcss
+
+npm install --save-dev @typescript-eslint/eslint-plugin
+
+# {
+#   "plugins": ["@typescript-eslint"],
+# }
+
+# {
+#   "extends": ["plugin:@typescript-eslint/recommended"]
+# }
