d5259b01e0d0d4a2fa7d570e07ed8c6a2328b211
GS Gemini <me.gsbit@hsx5.com>
2024-11-18 17:36:01 -0500

    adding unstable

--- mkshell-nodejs.nix
+++ mkshell-nodejs.nix
@@ -1,3 +1,11 @@
+
+# Get the current stable Nixpkgs
+nix flake show "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz"
+
+# Get the current unstable Nixpkgs
+nix flake show "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz"
+
+
         default = pkgs.mkShell {
           hardeningDisable = ["all"];
           packages = with pkgs; [ 
